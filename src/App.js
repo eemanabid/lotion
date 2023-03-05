85051bd725c911d877f8aee0d678ecba22dd8be8
eemanabid <abideeman@gmail.com>
2023-03-04 22:47:24 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <h1>Lotion</h1>;
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+function App(){
+  //Getter = notes, Setter = setNotes
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuidv4(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
+  const onDeleteNote = () => {
+    
+  }
+
+  return <div className = "App">
+
+    <Sidebar notes={notes} onAddNote={onAddNote} onDeleteNote={onDeleteNote}/>
+    <Main />
+  </div>
+
 }
 
 export default App;
